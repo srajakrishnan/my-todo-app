b4942c0e1cf530479ca22768654271612b6f61f2
sraja <srajakrishnancanada@gmail.com>
2024-02-08 22:55:37 +0530

    static web page creation

--- web.py
+++ web.py
@@ -12,7 +12,13 @@ st.title("My Todo WebApp")
 st.subheader("This is todo list page")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a todo",placeholder="Add a todo",on_change=add_todo,key='new_todo')
+st.session_state
